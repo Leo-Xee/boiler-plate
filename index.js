d096e2bb4f7cc98ec2d877ee84fcbf390dee198f
Leo-Xee <jang.xmin@gmail.com>
2021-11-01 22:35:31 +0900

    [FIX] DB key값 숨기기

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require('express');
+const bodyParse = require('body-parser');
 const app = express();
 const port = 3000;
 
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+app.use(bodyParse.urlencoded({ extended: true }));
+
+app.use(bodyParse.json());
+
 const mongoose = require('mongoose');
-const URI =
-  'mongodb+srv://leo:abcd1234@boiler-plate.cz8ay.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const URI = config.mongoURI;
 mongoose
   .connect(URI)
   .then(() => console.log('MongoDB Connected...'))
   .catch((e) => console.log(e));
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World, 안녕하세요!');
+});
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+      userInfo,
+    });
+  });
 });
 
 app.listen(port, () => {
