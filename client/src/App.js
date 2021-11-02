26e3bce01c0dd08d934a216c9e4dea32f0c82b88
Leo-Xee <jang.xmin@gmail.com>
2021-11-02 16:06:09 +0900

    [ADD] router 추가 및 적용

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import LandingPage from './components/views/LandingPage/LandingPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/register" component={RegisterPage} />
+        <Route exact path="/login" component={LoginPage} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
